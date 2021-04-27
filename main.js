467c66d9cc687ab5576907f2b209021659572ed7
ouuoussama <51234577+ouuoussama@users.noreply.github.com>
2021-04-27 00:28:05 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,9 @@
-var another = document.getElementById('bg2');
-var parallax = document.getElementById('topone');
-window.addEventListener('scroll',function(){
-    let offset = window.scrollY;
-    parallax.style.backgroundPositionY= offset * -.5+ 'px';
-    another.style.backgroundPositionY= offset * -.7 + 'px';
+var button = document.querySelector(".navbar-toggler");
+var menu =document.querySelector("#navbarTogglerDemo01");
+button.addEventListener("click", function(event){
+    if (menu.style.display == "none") {
+        menu.style.display = "";
+    } else {
+        menu.style.display = "none";
+    }
 })
-
-
-///// navbar//('//
-
-var navbarSlid = function(){
-     var lines = document.querySelector('.lines');
-     var nav = document.querySelector('.nav-links')
-    var navlinks = document.querySelectorAll('.nav-links li');
-     lines.addEventListener('click', function(){
-        nav.classList.toggle('nav-active');
-          ///animation link
-     navlinks.forEach((link, index)=>{
-       if (link.style.animation){
-        link.style.animation= '';
-       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.3 }s`
-       }
-    })
-    lines.classList.toggle('toggle');
-     });
-   
-}
-navbarSlid();
